3f583e1f093197a3f10a2a4704404913539fc575
Projecoes <andrewcoelho2507@gmail.com>
2025-01-07 19:02:25 -0300

    feat: - Implementação de um filter estendendo OncePerRequestFilter para interceptação das requisições; - Utilização da biblioteca Auth0 java-jwt para validação dos tokens recebidos na API; - Processamento da autenticação da requisição utilizando a classe SecurityContextHolder do Spring; - Configuração para liberar ou restringir requisições com base na URL e no verbo HTTP; - Adição de novos tratamentos de erros para melhorar a robustez da API.

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,8 +1,10 @@
 package med.voll.api.infra.security;
 
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,20 +14,36 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Autowired
+    private SecurityFilter securityFilter;
 
-    //devolve um objeto para o Spring para fazer injeções de dependencias com @Autowired
+    /**
+     * Configura a cadeia de filtros de segurança para a aplicação utilizando o Spring Security.
+     * A configuração inclui:
+     * - Desabilitação da proteção contra CSRF, uma vez que a aplicação pode ser uma API stateless
+     *   e não utiliza sessões tradicionais que requerem proteção contra CSRF.
+     * - Configuração do gerenciamento de sessões para "STATELESS", indicando que a aplicação
+     *   não manterá sessões de usuário entre requisições.
+     * - Permissão explícita para requisições POST no endpoint "/login" sem autenticação.
+     * - Exigência de autenticação para todas as outras requisições.
+     */
     @Bean
-    //metodo desabilita o csrf (segurança) e habilita o modo STATELESS para trabalhar com tokens
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        return http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
